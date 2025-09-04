6e61529690f0b0002a8d4891d7fc638845ff5f2a
dariodelagb <dar.jeria@duocuc.cl>
2025-09-04 16:28:29 -0400

    Validaciones JS contacto y ingresar

---
+++ Profecto FS2/assets/js/script.js
@@ -0,0 +1,36 @@
+function checkChar(text, maxChar, minChar, elementId, errorMin, errorMax){
+    if(text.length < minChar){
+        document.getElementById(elementId).innerHTML = errorMin;
+    }else if (text.length > maxChar){
+        document.getElementById(elementId).innerHTML = errorMax;
+    }else{
+        document.getElementById(elementId).innerHTML = "";
+    }
+}
+
+const Domains = ['gmail.com', 'duoc.cl', 'profesor.duoc.cl'];
+function checkEmail(email) {
+    // Check if email contains @
+    const atIndex = email.indexOf('@');
+    if (atIndex === -1) { //Si no hay simbolo '@', ejecutar
+        document.getElementById("domainError").innerHTML = "Ingrese un email válido: (@duoc.cl, @profesor.duoc.cl, @gmail.com)";
+        return false;
+    }
+
+    const domain = email.slice(atIndex + 1).toLowerCase();
+
+    if(Domains.includes(domain)){//Si pasa todas las validaciones, ejecutar
+        document.getElementById("domainError").innerHTML = "";
+
+        return true;
+    }else{//Si no tiene un dominio valido, ejecutar
+        document.getElementById("domainError").innerHTML = "Ingrese un email válido: (@duoc.cl, @profesor.duoc.cl, @gmail.com)";
+        return false;
+    }
+}
+
+function enviarForm(text, elementId, errorMsg){
+    if(text.length < 1){
+        document.getElementById(elementId).innerHTML = errorMsg;
+    }
+}
